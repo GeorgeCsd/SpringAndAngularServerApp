d6fc06b754452726ff997a2071b61c82da2189d1
GeorgeCsd <giorgostg7@yahoo.com>
2023-04-21 02:46:56 +0300

    Spinng boot application that gets,deletes and updates object of type Server in a database

---
+++ loginApplication/src/main/java/com/example/loginApplication/repo/ServerRepo.java
@@ -0,0 +1,9 @@
+package com.example.loginApplication.repo;
+
+
+import com.example.loginApplication.model.Server;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ServerRepo extends JpaRepository<Server, Long> {
+    Server findByIpAddress(String ipAddress);
+}
